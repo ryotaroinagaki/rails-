83274a931a7026f791abe52ccc6181735bfd2f5f
github ryotaroinagaki <aaadddryo@gmail.com>
2023-04-12 11:19:22 +0900

    add admin_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_11_232508) do
+ActiveRecord::Schema.define(version: 2023_04_12_005814) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_04_11_232508) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
